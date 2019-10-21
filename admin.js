fc7ad96f7ef0f85b18448a6d0d0efb8339a11319
BanerjeeRaunak99 <banerjeeraunak97@gmail.com>
2019-10-21 16:07:49 +0530

    json file created for dataset

--- admin.js
+++ admin.js
@@ -1,4 +1,6 @@
+'use strict';
 const admin = require('firebase-admin');
+const fs = require('fs');
 const functions = require('firebase-functions');
 var serviceAccount = require('./pingu-c43b5-firebase-adminsdk-51hbn-8382c08afc.json');
 admin.initializeApp({
@@ -8,13 +10,33 @@ admin.initializeApp({
 
 
 let db = admin.firestore();
-db.collection('locations').get()
-  .then((snapshot) => {
-    snapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
+// db.collection('locations').get()
+//   .then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, '=>', doc.data());
+//     });
+//   })
+//   .catch((err) => {
+//     console.log('Error getting documents', err);
+//   });
+let citiesRef = db.collection('locations');
+let query = citiesRef.where('flag', '==', "v").get()
+  .then(snapshot => {
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }  
+    fs.writeFileSync('data.json','{')
+    snapshot.forEach(doc => {
+      var data = JSON.stringify(doc.data(), null, 2);
+      console.log(data)
+      fs.appendFileSync('data.json', data);
+      fs.appendFileSync('data.json', ',');
+
+      //console.log(doc.data());
     });
+    fs.appendFileSync('data.json','}')
   })
-  .catch((err) => {
+  .catch(err => {
     console.log('Error getting documents', err);
   });
- 
